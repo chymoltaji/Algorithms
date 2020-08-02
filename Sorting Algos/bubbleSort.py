7fb7f2e2b6bca0074e7603cf9cc73e5f50f04b17
cheyennemoltaji <moltaji.c@husky.neu.edu>
2020-08-01 23:32:55 -0700

    Create bubble sort
    and edit a bit of insertion sort

--- Sorting Algos/bubbleSort.py
+++ Sorting Algos/bubbleSort.py
@@ -1,10 +1,17 @@
-def sort(array):
+def bubbleSort(array):
+    n = len(array)
+    edit = False
+    for i in range(1,n):
+        if array[i]<array[i-1]:
+            array[i], array[i-1] = array[i-1], array[i]
+            edit = True
+    if edit == True:
+        bubbleSort(array)
     return array
-
 arr = [0, 2, 15, 2017, 1, 1, 17, 3, 7, 1, 2, 3, 9, 22]
 correct = [0, 1, 1, 1, 2, 2, 3, 3, 7, 9, 15, 17, 22, 2017]
 
-result = sort(arr)
+result = bubbleSort(arr)
 if result == correct:
     print(f"CORRECT:\t{arr}\nSorted to:\t{result}")
 else:
--- Sorting Algos/insertionSort.py
+++ Sorting Algos/insertionSort.py
@@ -1,16 +1,17 @@
-def sort(array):
+def insertionSort(array):
     n = len(array)
     for i in range(1,n):
         for j in range(n):
             if array[i] <= array[j]:
                 value = array.pop(i)
                 array.insert(j, value)
+                print(array)
     result = array
     return result
 
 arr = [0, 2, 15, 2017, 1, 1, 17, 3, 7, 1, 2, 3, 9, 22]
 correct = [0, 1, 1, 1, 2, 2, 3, 3, 7, 9, 15, 17, 22, 2017]
-result = sort(arr)
+result = insertionSort(arr)
 if result == correct:
     print(f"CORRECT: Sorted to\t{result}")
 else:
