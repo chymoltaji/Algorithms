506ef6f36f2519bfc43a0f3bef4090e04df51c48
cheyennemoltaji <moltaji.c@husky.neu.edu>
2020-08-09 14:48:03 -0700

    Update to include profit

--- DecreasingPercent2.py
+++ DecreasingPercent2.py
@@ -1,5 +1,5 @@
-def splitpercent(n):
-    total = 100
+def split_percent(profit, n):
+    total = 1-profit
     unit = total/n
     mid_low = n//2
     if n % 2 == 0:
@@ -14,12 +14,10 @@ def splitpercent(n):
             prize_list[i] = unit + abs(mid_low-i)*(total/n**2)
         elif i>mid_high:
             prize_list[i] = unit - abs(mid_high-i)*(total/n**2)
+    return prize_list
 
-    print(prize_list)
-    print("SUM: "+str(sum(prize_list)))
-
-splitpercent(3)
-splitpercent(5)
-splitpercent(10)
-splitpercent(20)
-splitpercent(25)
+print(split_percent(0.4, 3))
+print(split_percent(0.3, 5))
+print(split_percent(0.1, 10))
+print(split_percent(0.2, 20))
+print(split_percent(0.2, 25))
