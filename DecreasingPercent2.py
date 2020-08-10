cbfbe1025aba88172baa76453288cc58456dfe55
cheyennemoltaji <moltaji.c@husky.neu.edu>
2020-08-10 15:38:10 -0700

    Minimal update

--- DecreasingPercent2.py
+++ DecreasingPercent2.py
@@ -2,18 +2,15 @@ def split_percent(profit, n):
     total = 1-profit
     unit = total/n
     mid_low = n//2
-    if n % 2 == 0:
-        mid_high = mid_low - 1
-    else:
-        mid_high = mid_low
+    mid_high = mid_low - 1 if n % 2 == 0 else mid_low
     prize_list = [0]*n
-    prize_list[mid_low] = unit
-    prize_list[mid_high] = unit
+    prize_list[mid_low] = round(unit, 4)
+    prize_list[mid_high] = round(unit, 4)
     for i in range(n):
-        if i<mid_low:
-            prize_list[i] = unit + abs(mid_low-i)*(total/n**2)
-        elif i>mid_high:
-            prize_list[i] = unit - abs(mid_high-i)*(total/n**2)
+        if i < mid_low:
+            prize_list[i] = round(unit + abs(mid_low-i)*(total/n**2), 4)
+        elif i > mid_high:
+            prize_list[i] = round(unit - abs(mid_high-i)*(total/n**2), 4)
     return prize_list
 
 print(split_percent(0.4, 3))
